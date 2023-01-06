d477b9da878d983b286ca2300162210f49cf64be
Agourrame <newhamza2019@gmail.com>
2023-01-06 01:24:40 +0100

    add the last virsion

--- android/app/src/main/java/com/app/hello/MainActivity.java
+++ android/app/src/main/java/com/app/hello/MainActivity.java
@@ -2,8 +2,17 @@ package com.app.hello;
 
 
 import com.getcapacitor.BridgeActivity;
-
+import android.os.Bundle;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    registerPlugin(GoogleAuth.class);
+
+  }
 
 }
